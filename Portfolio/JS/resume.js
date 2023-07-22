f0f03dea1972c6cdc3778c8d75139cfb258abc44
Mrdivyendalwadi <114602971+Divyen-Dalwadi2000@users.noreply.github.com>
2023-07-22 22:49:38 +0530

    Update resume.js

--- Portfolio/JS/resume.js
+++ Portfolio/JS/resume.js
@@ -1,9 +1,8 @@
 $(document).ready(() => {
     $("#resume").click(() => {
         const link = document.createElement('a');
-        link.href = "https://gujaratuniversityac-my.sharepoint.com/:w:/g/personal/mca2023_6_gujaratuniversity_ac_in/EYk0U7vHSAxEtCWsLgeA-ZwB3nzKiku3Oyw7I2sL_ClHdg?e=XaxcbO";
-        link.style.display = "none";
-        link.download = "Divyen.docx";
+        link.href = "F:\Project\portfolio\cv.pdf";
+        link.download = "cv.pdf";
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
